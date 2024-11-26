e5c7a834d0c2f1f5273359bad13b4f07a64fa0c7
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-26 17:54:05 -0300

    feat: create main function

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import cv2
 import streamlit as st
 
 
-st.title("Editor de Imagem")
+def main():
+    st.title("OpenCV Data App")
+    st.subheader(
+        "Este aplicativo web permite integrar processamento de imagens com OpenCV"
+    )
+    st.text("Streamlit com OpenCV")
+
+    image = cv2.imread("img/placeholder.png")
+
+    st.text("Imagem original")
+    st.image(image)
+
+
+if __name__ == "__main__":
+    main()
